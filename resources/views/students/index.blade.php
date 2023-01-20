9f6c722086ac0edfd7a3e29b48945c51110ec18d
Rommel Ramos <ramosrommel2001@gmail.com>
2023-01-20 13:28:10 +0800

    Utilized Tailwind CSS

--- resources/views/students/index.blade.php
+++ resources/views/students/index.blade.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <ul>
+    <ul class="">
         @foreach ($students as $student)
             <li>{{ $student->gender }} {{ $student->gender_count }}</li>
         @endforeach
